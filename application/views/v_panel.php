bc5092bf7d472b2e94ccadf540e2ef2e0743ef3a
Cesar Nicho <cesar1992.12.16@gmail.com>
2013-05-13 11:09:30 -0500

    Pie Mejorado

--- application/views/v_panel.php
+++ application/views/v_panel.php
@@ -24,7 +24,7 @@
 				<?php $this->load->view('panel/nav_left');?>
 			</article>
 			<section class="body_right"></section>
-			<footer class="footer" style="color: white;"><center>©2013 Universidad Nacional José Faustino Sánchez Carrión<p>Facultad de Ingenieria - Ingenieria de Sistemas - Sistema Web Para el Control y Gestion de Tesis<p></footer>
+			<footer class="footer" style="color: white;"><center>©2013 Universidad Nacional Jos&eacute; Faustino S&aacute;nchez Carri&oacute;n<p>Facultad de Ingenier&iacute;a - Ingenier&iacute;a de Sistemas - Sistema Web Para el Control y Gesti&oacute;n de Tesis<p></footer>
 		</section>
 	</body>
 </html>
