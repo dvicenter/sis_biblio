c6d55d8fca8a011987b5f2f1e3250dfb0ac27eb8
diego vicente rojas <diego_3_669@hotmail.com>
2013-05-14 02:07:11 -0500

    Quitando "index.php" de la URL

--- application/config/config.php
+++ application/config/config.php
@@ -1,6 +1,6 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 $config['base_url']	= 'http://localhost/sis_biblio/';
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 $config['uri_protocol']	= 'AUTO';
 $config['url_suffix'] = '';
 $config['language']	= 'english';
