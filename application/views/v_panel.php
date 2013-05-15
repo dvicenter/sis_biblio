5e9ce2937fe07618ff4985f33deb04a516718884
Deyvis Valladares <deyj_9@hotmail.com>
2013-05-15 00:41:49 -0500

    cabecera con sesion

--- application/views/v_panel.php
+++ application/views/v_panel.php
@@ -18,6 +18,18 @@
 	<body>
 		<section class="body">
 			<header class="header"> <img src="\sis_biblio\resource\img\header\cabecera.jpg" width="100%" height="89" alt="UNJFSC-Gestion de Biblioteca Central"> 
+			   <!--ul class="position_session">
+			   <li style="margin-top: -75px;">Deyvis Valladares Loza</li>
+			   </ul-->
+			   <ul class="nav pull-right position_session">
+			   <li style="margin-top: 10px;"><img class="img-rounded" width="48px" style="width: 30px; height: 30px"></li>
+				<li><a href="#">Cesar Nicho Diaz </a></li>
+				<li class="divider-vertical"></li>
+				<li class="dropdown"><a href="javascript:salir();" class="dropdown-toggle" data-toggle="dropdown">Cerrar Sesión </a>
+				 
+				</li>
+			   </ul>
+					   
 			</header>
 			<nav class="nav_top">
 				<?php $this->load->view('panel/nav_top');?>
