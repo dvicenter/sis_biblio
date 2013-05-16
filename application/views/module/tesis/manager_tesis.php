234a856d1722c62d1c89edc8b7fdec583cb2032c
Cesar NIcho <cesar1992.12.16@gmail.com>
2013-05-16 00:16:52 -0500

    datos

--- application/views/module/tesis/manager_tesis.php
+++ application/views/module/tesis/manager_tesis.php
@@ -1,3 +1,17 @@
+<?php  $datos=array(
+			array($titulo='Implementaci&oacute;n de un sistema de informaci&oacute;n',$bachiller='Mariafernanda Nicho Alvarado',$asesor='Ing. Cesar Nicho Diaz',$anio='2013'),
+			array($titulo='Sistema web a medidas',$bachiller='Alexander Nicho Alvarado',$asesor='Ing. Deyvis Valladares',$anio='2013'),
+			array($titulo='Consultoria a la empresa FutgolPeru',$bachiller='Piero Alva Solo',$asesor='Ing. Kelly Grados Manrique',$anio='2013'),
+			array($titulo='Creaci&oacute;n del sistema web',$bachiller='Lionel Messi Ronaldo',$asesor='Ing. Karina Gonzales Angulo',$anio='2013'),
+			array($titulo='Implementaci&oacute;n de un sistema de informaci&oacute;n',$bachiller='Mariafernanda Nicho Alvarado',$asesor='Ing. Cesar Nicho Diaz',$anio='2013'),
+			array($titulo='Sistema web a medidas',$bachiller='Alexander Nicho Alvarado',$asesor='Ing. Deyvis Valladares',$anio='2013'),
+			array($titulo='Consultoria a la empresa FutgolPeru',$bachiller='Piero Alva Solo',$asesor='Ing. Kelly Grados Manrique',$anio='2013'),
+			array($titulo='Creaci&oacute;n del sistema web',$bachiller='Lionel Messi Ronaldo',$asesor='Ing. Karina Gonzales Angulo',$anio='2013'),
+			array($titulo='Implementaci&oacute;n de un sistema de informaci&oacute;n',$bachiller='Mariafernanda Nicho Alvarado',$asesor='Ing. Cesar Nicho Diaz',$anio='2013'),
+			array($titulo='Sistema web a medidas',$bachiller='Alexander Nicho Alvarado',$asesor='Ing. Deyvis Valladares',$anio='2013'),
+			array($titulo='Consultoria a la empresa FutgolPeru',$bachiller='Piero Alva Solo',$asesor='Ing. Kelly Grados Manrique',$anio='2013'),
+			array($titulo='Creaci&oacute;n del sistema web',$bachiller='Lionel Messi Ronaldo',$asesor='Ing. Karina Gonzales Angulo',$anio='2013')
+			);?>
 <div id="module_tesis">
 <table class="table table-striped table-bordered table-condensed">
   <thead>
@@ -11,16 +25,18 @@
     </tr>
   </thead>
   <tbody>
+  <?php foreach($datos as $dato){?>
     <tr>
       <td class="check_table"><input type="checkbox"/></td>
-      <td>Implementaci&oacute;n de un sistema de informaci&oacute;n</td>
-      <td>Mariafernanda Nicho Alvarado</td>
-      <td>Ing. Cesar Nicho Diaz</td>
-      <td>2013</td>
+      <td><?php echo $dato[0];?></td>
+      <td><?php echo $dato[1];?></td>
+      <td><?php echo $dato[2];?></td>
+      <td><?php echo $dato[3];?></td>
       <td>
       	<button class="btn"><i class="icon-pencil"></i></button>
       </td>
     </tr>
+    <?php }?>
   </tbody>
 </table>
 </div>
