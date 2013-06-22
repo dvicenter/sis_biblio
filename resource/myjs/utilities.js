82d5706babc2778d623cfcf1acf6bd39db0f615b
Cesar NIcho <cesar1992.12.16@gmail.com>
2013-06-22 18:05:54 -0500

    Login_Estatico

--- resource/myjs/utilities.js
+++ resource/myjs/utilities.js
@@ -5,11 +5,15 @@ $(document).ready(function(){
 		active_consult('.consult_asesor', '.consult_student');
 	});
 <<<<<<< HEAD
+<<<<<<< HEAD
 	function active_consult(active,desactive)
 	{	$(active).addClass('active_consult');
 		$(desactive).removeClass('active_consult');
 	}
 	 
+=======
+	
+>>>>>>> 378268bb6128bdc67bb481f441a8925a7d0cb35a
 =======
 	
 >>>>>>> 378268bb6128bdc67bb481f441a8925a7d0cb35a
