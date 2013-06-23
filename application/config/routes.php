bb8e078b85d558295193f6b1a9a037840d0af96d
Victor Morales <victor_flores_5@hotmail.com>
2013-06-23 14:14:22 +0200

    Listar,Insertar-Rol

--- application/config/routes.php
+++ application/config/routes.php
@@ -1,8 +1,13 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
-$route['default_controller'] = "CCPanel";
+$route['default_controller'] = "CCLogin";
 $route['404_override'] = '';
 $route['module/([a-zA-Z_-]+)/([a-zA-Z_-]+)']='CCPanel/ubication_module/$1/$2';
 $route['module/([a-zA-Z_-]+)']='CCPanel/control_panel/$1';
+<<<<<<< HEAD
 //$route['module/([a-zA-Z_-]+)']='CCPanel/control_panel/$1';
+=======
+$route['acceder/sistema']='CCLogin/index';
+$route['cerrar_sesion']='CCLogin/cerrar_sesion';
+>>>>>>> dd9a535af18ad83e67c39bca029aa8c1fe7e087e
 /* End of file routes.php */
 /* Location: ./application/config/routes.php */
