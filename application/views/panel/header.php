c6d02c3a7f759495ad081436f11ad8baa0d6cdbc
Cesar NIcho <cesar1992.12.16@gmail.com>
2013-06-23 21:00:12 -0500

    cerrar sesion

--- application/views/panel/header.php
+++ application/views/panel/header.php
@@ -1,5 +1,5 @@
 <a href="<?php echo base_url();?>"><img style="height: 100%;" src="\sis_biblio\resource\img\header\cabecera.jpg" width="100%" height="100%" alt="UNJFSC-Gestion de Biblioteca Central"></a> 
-<ul class="pull-right position_session">
+<ul class="nav pull-right position_session">
 	<li style="margin-top: 10px;"><img class="img-rounded" width="48px" style="width: 30px; height: 30px"></li>
 	<li><a href="javascript:;"><?php echo $usuario;?> </a></li>
 	<li style="margin-left: 10px;"><a href="<?php echo base_url()?>cerrar_sesion">Cerrar Sesi&oacute;n</a></li>
