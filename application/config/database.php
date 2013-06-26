63373ead499a2ecf8fcc3a0336144f73d9a04f50
Cesar NIcho <cesar1992.12.16@gmail.com>
2013-06-26 15:57:59 -0500

    database

--- application/config/database.php
+++ application/config/database.php
@@ -2,14 +2,14 @@
 $active_group = 'default';
 $active_record = TRUE;
 
-$db['default']['hostname'] = 'moralesflores.com';
+/*$db['default']['hostname'] = 'moralesflores.com';
 $db['default']['username'] = 'moralesf';
 $db['default']['password'] = 'leavemealoNE18:*';
-$db['default']['database'] = 'moralesf_sis_biblio';
-/*$db['default']['hostname'] = 'localhost';
+$db['default']['database'] = 'moralesf_sis_biblio';*/
+$db['default']['hostname'] = 'localhost';
 $db['default']['username'] = 'root';
 $db['default']['password'] = '12345';
-$db['default']['database'] = 'sis_biblio';*/
+$db['default']['database'] = 'sis_biblio';
 $db['default']['dbdriver'] = 'mysqli';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = TRUE;
