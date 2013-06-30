9a10c1f3640e9ddb448f0ae70b0630eba9fa37fe
Deyvis Valladares <deyj_9@hotmail.com>
2013-06-30 16:38:51 -0500

    avance pag

--- application/controllers/manager/ccaccion.php
+++ application/controllers/manager/ccaccion.php
@@ -16,41 +16,47 @@ class Ccaccion extends CI_Controller
     
     function listar()
     {
-        $config['base_url'] = base_url().'manager/ccaccion';
+         $config['base_url'] = base_url().'manager/ccaccion/listar/';
         $config['total_rows'] = $this->cd_accion->num_accion();
-        $config['per_page'] = 10;
-    //  $config['uri_segment'] = 3;
+        $config['per_page'] = 5;
         $config['num_links'] = 5;
-        $config['page_query_string'] = TRUE;
-        // $config['use_page_numbers'] = TRUE;
-        $config['query_string_segment'] = 'page';
-         
-        $config['full_tag_open'] = '<div class="pagination"><ul>';
-        $config['full_tag_close'] = '</ul></div><!--pagination-->';
-         
-        $config['first_link'] = '&laquo; First';
-        $config['first_tag_open'] = '<li class="prev page">';
-        $config['first_tag_close'] = '</li>';
-         
-        $config['last_link'] = 'Last &raquo;';
-        $config['last_tag_open'] = '<li class="next page">';
-        $config['last_tag_close'] = '</li>';
-         
-        $config['next_link'] = 'Next &rarr;';
-        $config['next_tag_open'] = '<li class="next page">';
-        $config['next_tag_close'] = '</li>';
-         
-        $config['prev_link'] = '&larr; Previous';
-        $config['prev_tag_open'] = '<li class="prev page">';
-        $config['prev_tag_close'] = '</li>';
-         
-        $config['cur_tag_open'] = '<li class="active"><a href="">';
-        $config['cur_tag_close'] = '</a></li>';
-         
-        $config['num_tag_open'] = '<li class="page">';
-        $config['num_tag_close'] = '</li>';
-        // $config['display_pages'] = FALSE;
-        // 
+        $config['first_link'] = 'Primero';
+        $config['last_link'] = 'Ultimo';
+        $config['next_link'] = 'Siguiente';
+        $config['prev_link'] = 'Anterior';
+        $config['cur_tag_open'] = '<b class="actual">';
+        $config['cur_tag_close'] = '</b>';
+        $config['full_tag_open'] = '<div id="paginacion">';
+        $config['full_tag_close'] = '</div>';
+//        $config['page_query_string'] = TRUE;
+//        // $config['use_page_numbers'] = TRUE;
+//        $config['query_string_segment'] = 'page';
+//         
+//        $config['full_tag_open'] = '<div class="pagination"><ul>';
+//        $config['full_tag_close'] = '</ul></div><!--pagination-->';
+//         
+//        $config['first_link'] = '&laquo; First';
+//        $config['first_tag_open'] = '<li class="prev page">';
+//        $config['first_tag_close'] = '</li>';
+//         
+//        $config['last_link'] = 'Last &raquo;';
+//        $config['last_tag_open'] = '<li class="next page">';
+//        $config['last_tag_close'] = '</li>';
+//         
+//        $config['next_link'] = 'Next &rarr;';
+//        $config['next_tag_open'] = '<li class="next page">';
+//        $config['next_tag_close'] = '</li>';
+//         
+//        $config['prev_link'] = '&larr; Previous';
+//        $config['prev_tag_open'] = '<li class="prev page">';
+//        $config['prev_tag_close'] = '</li>';
+//         
+//        $config['cur_tag_open'] = '<li class="active"><a href="">';
+//        $config['cur_tag_close'] = '</a></li>';
+//         
+//        $config['num_tag_open'] = '<li class="page">';
+//        $config['num_tag_close'] = '</li>';
+        
         $this->pagination->initialize($config);
         $data = array('accion'=>$this->cd_accion->listar($config['per_page']),
         'paginacion'=>$this->pagination->create_links());
