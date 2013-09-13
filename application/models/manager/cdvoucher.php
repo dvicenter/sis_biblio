9d20e570812dad427e52a5b5e37ac184cae55f70
SusanMR <susan_c93@hotmail.com>
2013-09-13 17:47:14 -0500

    Mensaje voucher 95%

--- application/models/manager/cdvoucher.php
+++ application/models/manager/cdvoucher.php
@@ -2,7 +2,7 @@
 class Cdvoucher extends CI_Model{
 	function load_voucher($tipo_personeria,$codigo_universitario,$documento,$fecha)
 	{	$this->load->database();
-		$query=("INSERT INTO tbl_voucher (codigo_alumno,documento,tipo_persona,estado,fecha) VALUES ('".$codigo_universitario."','".$documento."',".$tipo_personeria.",'1','".$fecha."');");
+		$query=("INSERT INTO tbl_voucher (codigo_alumno,documento,tipo_persona,estado,fecha,fecha_registro) VALUES ('".$codigo_universitario."','".$documento."',".$tipo_personeria.",'1','".$fecha."',now());");
 		$this->db->query($query);
 		return true;
 		//$query->result()
